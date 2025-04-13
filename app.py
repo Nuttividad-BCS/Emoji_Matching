36449dda25c9eac8ccdb977fcfdf85a4353779a0
Kenneth <kennethsambayon@gmail.com>
2025-04-13 18:20:38 +0800

    Connected na both front end at model

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+from Model import analyze_text_with_emoji
+from flask import request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    with app.app_context():
-        return render_template('Main.html')
-
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    emoji_result = None
+    if request.method == 'POST':
+        user_text = request.form.get('text')
+        emoji_result = analyze_text_with_emoji(user_text)
+    return render_template('Main.html', emoji_result=emoji_result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
